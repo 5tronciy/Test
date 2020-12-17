b5ee93508f5f390cd0ffec8832738a91d719f429
Stronciy <gorovin-ne@ya.ru>
2020-12-17 15:36:12 +0300

    add Date and Time pickers

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+import TimePicker from "react-time-picker";
 import styles from "./Input.module.css";
 
 const InputConnected = () => {
   const onCreate = () => {};
+  const [dateValue, onChangeDate] = useState(undefined);
+  const [timeValue, onChangeTime] = useState(undefined);
 
-  return <Input onCreate={onCreate} />;
+  return (
+    <Input
+      onCreate={onCreate}
+      onChangeDate={onChangeDate}
+      dateValue={dateValue}
+      onChangeTime={onChangeTime}
+      timeValue={timeValue}
+    />
+  );
 };
 
 interface Props {
   onCreate: () => void;
+  onChangeDate: (event: any) => void;
+  dateValue: Date | undefined;
+  onChangeTime: (event: any) => void;
+  timeValue: any;
 }
 
-export const Input = ({ onCreate }: Props) => {
+export const Input = ({
+  onCreate,
+  onChangeDate,
+  dateValue,
+  onChangeTime,
+  timeValue,
+}: Props) => {
   return (
     <div className={styles.content}>
       <input
@@ -19,8 +41,13 @@ export const Input = ({ onCreate }: Props) => {
         className={styles.textBox}
         placeholder="Любой текст..."
       />
-      <input type="date" className={styles.dateBox} placeholder="Дата" />
-      <input type="time" className={styles.timeBox} placeholder="Время" />
+
+      <div className={styles.dateBox} placeholder="Дата">
+        <DatePicker onChange={onChangeDate} value={dateValue} />
+      </div>
+      <div className={styles.timeBox} placeholder="Время">
+        <TimePicker onChange={onChangeTime} value={timeValue} />
+      </div>
       <button type="button" className={styles.create} onClick={onCreate}>
         <span className={styles.buttonCreateName}>Создать</span>
       </button>
